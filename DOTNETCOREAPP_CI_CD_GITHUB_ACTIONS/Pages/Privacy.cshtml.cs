f275e316bfd27afb8f3feb99f4f3cd2f37879f0f
PraveenSuggula <you@example.compraveenkumar.suggula0@gmail.com>
2024-03-03 00:39:55 -0600

    Add project files.

---
+++ DOTNETCOREAPP_CI_CD_GITHUB_ACTIONS/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DOTNETCOREAPP_CI_CD_GITHUB_ACTIONS.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
